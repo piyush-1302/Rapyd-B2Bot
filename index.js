1efccfa40db3413899f001d4eb617f3067ef9313
Shubham Prasad <shubhamprasad63@gmail.com>
2022-05-24 22:55:12 +0530

    Change message

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
 app.get("/", (req, res) => {
   res.json({
     msg: "Hello world",
@@ -11,8 +14,9 @@ app.get("/", (req, res) => {
 });
 
 app.post("/order", (req, res) => {
+  const cartValue = req.body.cartvalue;
   res.json({
-    msg: "Order processed successfully",
+    msg: `Order processed successfully for an order of $${cartValue}`,
   })
 })
 
