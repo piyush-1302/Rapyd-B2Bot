2299a8d1a3f462e5ccd02a23635cc9efa462419d
Piyush Kumar <piyushkumar@Piyushs-MacBook-Air.local>
2022-05-24 23:24:28 +0530

    add endpoint

--- index.js
+++ index.js
@@ -20,4 +20,13 @@ app.post("/order", (req, res) => {
   })
 })
 
+app.get("/retrive-outstanding-payment", (req, res) => {
+  // const cartValue = req.body.cartvalue;
+  let paymentInfo1 = {merchantId:"950ae8c6-78", amount:120, dueDate:new Date(2022, 05, 29), currency:"INR"};
+  let paymentInfo2 = {merchantId:"950ae8c6-78", amount:501, dueDate:new Date(2022, 05, 30), currency:"INR"};
+  let paymentInfo3 = {merchantId:"950ae8c6-78", amount:1102, dueDate:new Date(2022, 05, 29), currency:"INR"};
+  const outstandingPayments = [paymentInfo1,paymentInfo2,paymentInfo3];
+  res.json({outstandingPayments})
+})
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
