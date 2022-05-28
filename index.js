bbafcb85c4e8ff02aab9f55faa3793f88725c7f8
Piyush Kumar <piyushkumar@Piyushs-MacBook-Air.local>
2022-05-28 23:26:51 +0530

    add all APIs

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 //import { MongoClient } from "mongodb";
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const { getRapyPayCheckoutId } = require("./signature");
-
+const crypto = require('crypto');
 const express = require("express");
+const { randomUUID } = require("crypto");
 
 const app = express();
 
@@ -23,7 +24,7 @@ async function mongoDb() {
 
 /*
  Request {
-	"cartvalue": "@cartvalue",
+  "cartvalue": "@cartvalue",
   "paynow": true,
   "user_id": "1",
   "merchant_id": "1",
@@ -53,32 +54,47 @@ Database Storage :
 */
 app.post("/order", async (req, res) => {
 
-const uri =
-  "mongodb+srv://shivam:shivam@cluster0.173wr.mongodb.net/payments?retryWrites=true&w=majority";
+  const uri =
+    "mongodb+srv://shivam:shivam@cluster0.173wr.mongodb.net/payments?retryWrites=true&w=majority";
 
   const client = await MongoClient.connect(uri);
   const db = client.db();
   const collection = db.collection("checkout");
-
-  if(req.body.paynow === true){
-    const checkoutId = await getRapyPayCheckoutId(req, res);
+  let checkoutId = ''
+  const paymentId = Math.floor(Math.random() * 1000000) + 1
+  if (req.body.paynow === true) {
+    checkoutId = await getRapyPayCheckoutId(req, res);
     console.log(checkoutId)
     collection.insertOne({
-       // paymentId: ":paymentId",
-       user_id: req.body.user_id,
-       merchant_id: req.body.merchant_id,
-       cartvalue: req.body.cartvalue,
-       checkoutId: checkoutId,
-       dueDate: new Date(new Date().getTime()+(45*24*60*60*1000)),
-       isPaid:"true",
-       currency: req.body.currency
-     });
+      paymentId: paymentId,
+      user_id: req.body.user_id,
+      merchant_id: req.body.merchant_id,
+      cartvalue: req.body.cartvalue,
+      checkoutId: checkoutId,
+      dueDate: new Date().getTime(),
+      humanDate: new Date(new Date().getTime()),
+      isPaid: "true",
+      currency: req.body.currency
+    });
+  } else {
+    collection.insertOne({
+      paymentId: paymentId,
+      user_id: req.body.user_id,
+      merchant_id: req.body.merchant_id,
+      cartvalue: req.body.cartvalue,
+      //  checkoutId: checkoutId,
+      dueDate: new Date().getTime() + (45 * 24 * 60 * 60 * 1000),
+      humanDate: new Date(new Date().getTime() + (45 * 24 * 60 * 60 * 1000)),
+      isPaid: "false",
+      currency: req.body.currency
+    });
   }
 
 
   const cartValue = req.body.cartvalue;
   res.json({
-    msg: `Order processed successfully for an order of $${req.body.cartvalue}`,
+    checkoutId: checkoutId,
+    paymentId: paymentId
   });
 });
 
@@ -94,28 +110,53 @@ Response {
 }
 1. Fetch checkoutId from DB with date range and userId
 */
-app.get("/retrive-outstanding-payment", (req, res) => {
+app.post("/retrive-outstanding-payment", async (req, res) => {
   // const cartValue = req.body.cartvalue;
-  let paymentInfo1 = {
-    merchantId: "950ae8c6-78",
-    amount: 120,
-    dueDate: new Date(2022, 05, 29),
-    currency: "INR",
-  };
-  let paymentInfo2 = {
-    merchantId: "950ae8c6-78",
-    amount: 501,
-    dueDate: new Date(2022, 05, 30),
-    currency: "INR",
-  };
-  let paymentInfo3 = {
-    merchantId: "950ae8c6-78",
-    amount: 1102,
-    dueDate: new Date(2022, 05, 29),
-    currency: "INR",
-  };
-  const outstandingPayments = [paymentInfo1, paymentInfo2, paymentInfo3];
-  res.json({ outstandingPayments });
+  const userId = req.body.user_id;
+  const currentDate = new Date().getTime();
+  const futureDate = new Date().getTime() + (2 * 24 * 60 * 60 * 1000);
+  const uri =
+    "mongodb+srv://shivam:shivam@cluster0.173wr.mongodb.net/payments?retryWrites=true&w=majority";
+  const client = await MongoClient.connect(uri);
+  const db = client.db();
+  const collection = db.collection("checkout");
+
+  var data = await collection
+    .find({ user_id: userId }, { dueDate: { $gt: currentDate, $lt: futureDate } })
+    .toArray();
+
+  // let paymentInfo1 = {
+  //   merchantId: "950ae8c6-78",
+  //   amount: 120,
+  //   dueDate: new Date(2022, 05, 29),
+  //   currency: "INR",
+  // };
+  // let paymentInfo2 = {
+  //   merchantId: "950ae8c6-78",
+  //   amount: 501,
+  //   dueDate: new Date(2022, 05, 30),
+  //   currency: "INR",
+  // };
+  // let paymentInfo3 = {
+  //   merchantId: "950ae8c6-78",
+  //   amount: 1102,
+  //   dueDate: new Date(2022, 05, 29),
+  //   currency: "INR",
+  // };
+  let r = []
+  for (let i = 0; i < data.length; i++) {
+    if ((data[i].isPaid === "false") && (data[i].dueDate < futureDate) && (data[i].dueDate >= currentDate)) {
+      r.push({
+        amount: data[i].cartvalue,
+        currency: data[i].currency,
+        merchantId: data[i].merchant_id,
+        dueDate: data[i].humanDate,
+        paymentId: data[i].paymentId
+      })
+    }
+  }
+  const outstandingPayments = r;
+  res.json({ outstandingPayments: r });
 });
 
 // /*
@@ -125,8 +166,40 @@ app.get("/retrive-outstanding-payment", (req, res) => {
 // }
 // */
 
-// app.post("/do-payment", (req,res) =>{
+app.post("/do-payment", async (req, res) => {
+  const payId = req.body.payId;
+  const uri =
+    "mongodb+srv://shivam:shivam@cluster0.173wr.mongodb.net/payments?retryWrites=true&w=majority";
+  const client = await MongoClient.connect(uri);
+  const db = client.db();
+  const collection = db.collection("checkout");
 
-// });
+  var data = await collection
+    .findOne({ paymentId: +payId });
+
+  var body = {
+    cartvalue: data.cartvalue,
+    currency: data.currency,
+    merchantId: data.merchant_id
+  }
+
+  var req1 = {
+    body
+  }
+
+  let checkoutId = await getRapyPayCheckoutId(req1, res);
+  console.log(checkoutId)
+  collection.findOneAndUpdate({ paymentId: +payId }, {
+    $set: {
+      checkoutId: checkoutId,
+      isPaid: "true"
+    }
+  });
+
+  res.json({
+    checkoutId: checkoutId
+  });
+
+});
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
